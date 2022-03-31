dfba4557b09d7dcf4fabfb49284f66639eeeb98a
Milena Michalska <milena.michalska@dac.digital>
2022-03-31 23:46:27 +0200

    added first dialogue lines

--- ComputerInteraction.gd
+++ ComputerInteraction.gd
@@ -1,7 +1,9 @@
 extends Area2D
 
 var active = false
-onready var chat = get_node("../PlanetChat")
+
+var windows = ["../PlanetChat", "", ""]
+var timelines = ['computer-timeline1', 'chat', 'projects', 'big-day', 'food']
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -12,9 +14,22 @@ func _process(delta):
 	$QMark.visible = active
 	
 func _input(event):
-	if event.is_action_pressed("ui_accept") and active:
-		chat.visible = true
+	if event.is_action_pressed("ui_accept") and active and len(timelines) > 0:
+		# if get_node_or_null('DialogNode') == null:
+		get_tree().paused = true
+		var dialog = Dialogic.start(timelines[0])
+		dialog.pause_mode = Node.PAUSE_MODE_PROCESS
+		dialog.connect('timeline_end', self, 'unpause')
+		add_child(dialog)
 		pass
+		
+func unpause(timeline_name):
+	timelines.remove(0)
+	if windows[0] != "":
+		var window = get_node(windows[0])
+		window.visible = true
+	windows.remove(0)
+	get_tree().paused = false
 		
 	
 func _computer_on_body_entered(body):
