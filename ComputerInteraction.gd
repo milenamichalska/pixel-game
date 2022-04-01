a3770b2936dc343b7fe63eb9b9941a83b5b81281
Milena Michalska <milena.michalska@dac.digital>
2022-04-01 18:05:12 +0200

    first chapter finished

--- ComputerInteraction.gd
+++ ComputerInteraction.gd
@@ -1,9 +1,11 @@
 extends Area2D
 
 var active = false
+var end = false
 
-var windows = ["../PlanetChat", "", ""]
+var windows = ["../PlanetChat", "../ProjectWindow", "../BigMail", "../Pierogi", "","../f-days", "../f-days2", "../f-days3"]
 var timelines = ['computer-timeline1', 'chat', 'projects', 'big-day', 'food']
+var overlays = ['../darker1', '../darker2', '../darker3']
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -21,11 +23,26 @@ func _input(event):
 		dialog.pause_mode = Node.PAUSE_MODE_PROCESS
 		dialog.connect('timeline_end', self, 'unpause')
 		add_child(dialog)
+	elif event.is_action_pressed("ui_accept") and active:
+		if len(windows) > 0 and windows[0] != "":
+			if len(overlays) > 0:
+				var overlay = get_node(overlays[0])
+				overlay.visible = true
+				overlays.remove(0)
+			var window = get_node(windows[0])
+			window.visible = true
+			windows.remove(0)
+		else:
+			if not end:
+				timelines.append("anime")
+				end = true
+			else:
+				get_node('../Bg').visible = true
 		pass
 		
 func unpause(timeline_name):
 	timelines.remove(0)
-	if windows[0] != "":
+	if len(windows) > 0 and windows[0] != "":
 		var window = get_node(windows[0])
 		window.visible = true
 	windows.remove(0)
